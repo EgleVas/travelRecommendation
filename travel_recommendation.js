16340d6abf42f10a62c5a19d356047a49103e92e
EgleVas <egle.vasileviciute@gmail.com>
2024-02-05 06:58:46 -0500

    Update Contact US and Search functionality

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,77 +1,66 @@
 const btnSearch = document.getElementById("btnSearch");
 const btnReset = document.getElementById("btnReset");
-const btnSubmitForm = document.getElementById("btnSubmitForm");
-
-const countries = [];
 
 function resetSearch() {
     document.getElementById("searchInput").value = "";
-    document.getElementById("result").value = "";
-}
-
-function resetForm() {
-    document.getElementById("name").value = "";    
-    document.getElementById("email").value = "";
-    document.getElementById("message").value = "";
+    document.getElementById("result").innerHTML = "";
 }
 
 function searchGroup() {
     const input = document.getElementById('searchInput').value.toLowerCase();
     const resultDiv = document.getElementById('result');
-    resultDiv.innerHTML = '';
+    resultDiv.innerHTML = '<p class="text-with-background">YOUR SEARCH RESULTS</p>';
     fetch('travel_recommendation_api.json').then(response => response.json()).then(data => {
-        if (input === "temples") {
+        if (input.includes("templ")) {
             const temples = data.temples;
             if (temples) {
                 temples.forEach(item => {
-                    const name = item.name;
-                    const imageUrl = item.imageUrl;
-                    const description = item.description;
-                    resultDiv.innerHTML += `<img style="width: 100%" src="./images/${imageUrl}" alt="hjh">`;
-                    resultDiv.innerHTML += `<h2>${name}</h2>`;
-                    resultDiv.innerHTML += `<p>${description}</p>`;               
+                    addItem(item, resultDiv);
                 });                
             } else {
-                resultDiv.innerHTML = 'Temples not found.';        
+                resultDiv.innerHTML += '<div><h3 class="warning">Temples not found.</h3></div>';        
             }
         }
-        if (input === "beaches") {
+        if (input.includes("beach")) {
             const beaches = data.beaches;
             if (beaches) {
                 beaches.forEach(item => {
-                    const name = item.name;
-                    const imageUrl = item.imageUrl;
-                    const description = item.description;
-                    resultDiv.innerHTML += `<img style="width: 100%" src="./images/${imageUrl}" alt="hjh">`;
-                    resultDiv.innerHTML += `<h2>${name}</h2>`;
-                    resultDiv.innerHTML += `<p>${description}</p>`;
+                    addItem(item, resultDiv);
                 });                
             } else {
-                resultDiv.innerHTML = 'Beaches not found.';        
+                resultDiv.innerHTML += '<div><h3 class="warning">Beaches not found.</h3></div>';        
             }
         }
-        if (input === "countries") {
+        if (input.includes("count")) {
             const countries = data.countries;
             if (countries) {
                 countries.forEach(country => {
                     country.cities.forEach(item => {
-                        const name = item.name;
-                        const imageUrl = item.imageUrl;
-                        const description = item.description;
-                        resultDiv.innerHTML += `<img style="width: 100%" src="./images/${imageUrl}" alt="hjh">`;
-                        resultDiv.innerHTML += `<h2>${name}</h2>`;
-                        resultDiv.innerHTML += `<p>${description}</p>`;
+                        addItem(item, resultDiv);
                     })                                        
                 });                
             } else {
-                resultDiv.innerHTML = 'Countries not found.';        
+                resultDiv.innerHTML += '<div><h3 class="warning">Countries not found.</h3></div>';        
             }
         }
+        if (input === "" && !input.includes("templ") && !input.includes("beach") && !input.includes("count")){
+            resultDiv.innerHTML += '<div><h3 class="warning">Your search returned no results.<br>Search options are limited to countries, beaches and temples.<br>For your search you need enter at least 5 first characters.</h3></div>';
+        }
+        
     })
     .catch(error => {
         console.error('Error:', error);
-        resultDiv.innerHTML = 'An error occurred while fetching data.';
+        resultDiv.innerHTML = '<div><h3 class="warning">An error occurred while fetching data.</h3></div>';
     });
+}
+
+function addItem(item, resultDiv) {
+    const name = item.name;
+    const description = item.description;
+    const imageUrl = item.imageUrl;
+    resultDiv.innerHTML += `<h3 style="padding-top: 40px;">${name}</h3>`;
+    resultDiv.innerHTML += `<p>${description}</p>`;
+    resultDiv.innerHTML += `<img style="width: 100%" src="./images/${imageUrl}" alt="hjh">`;
 }
 
 btnSearch.addEventListener('click', searchGroup);
